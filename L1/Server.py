bcd7f6b705871f73d03a30338d127784a152c6b0
pedro <pedrinni14>
2020-12-04 16:49:55 +0100

    Modificacion en la clase Server.py

--- L1/Server.py
+++ L1/Server.py
@@ -6,7 +6,10 @@ Ice.loadSlice("icegauntlet.ice")
 import IceGauntlet
 import json
 import yaml
-from random import shuffle
+from random import shuffle , sample 
+import string 
+from glob import glob 
+
 
 class ServerI(IceGauntlet.RoomManager):
     def __init__(self, auth):
@@ -15,27 +18,43 @@ class ServerI(IceGauntlet.RoomManager):
 
 
     
+    def RoomExists(self,roomData):
+        '''
+        roomAlreadyExists method
+        '''
+        ficheros=glob('client-distrib-icegauntlet/assets/maps/*.json')
+        print('metodo')
+        try:
+            for i in ficheros:
+                with open(i,'r') as f:
+                    d=f.read()
+                d=json.loads(d)
+                if d['data']==roomData:
+                    return False
+            return True
+        except:
+            print("Eror, Not found data base")
+
     def Publish(self,token,roomData,current=None):
         datos=''
         roomData=yaml.load(roomData)
-               
+        room_name=roomData['room']
         if self.auth_server.isValid(token):
-            room=roomData["room"]
-            
+            print('El token es valido')
+            room=''.join(sample(string.ascii_letters, 8))
             ruta='client-distrib-icegauntlet/assets/maps/'+room+'.json'
-            if not os.path.isfile(ruta):            
+            if self.RoomExists(roomData['data']):      
                 with open(ruta,'w') as f:
                     json.dump(roomData, f, indent=4)
-                print('El token es valido')
-
                 try:
-                    with open('client-distrib-icegauntlet/maps.json') as f:
+                    with open('client-distrib-icegauntlet/publicmaps.json') as f:
                         maps=f.read()
                         maps=json.loads(maps)
                 except:
                     print("Eror, Not found data base")
 
-                maps[room]={'token' : token}
+                maps[room]={'token' : token ,
+                            'name' : room_name}
                 with open('client-distrib-icegauntlet/publicmaps.json','w') as f:
                     json.dump(maps, f, indent=4)
             else:
