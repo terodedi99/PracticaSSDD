38967d52fb7de0217aa7f6ee8ebece088f668331
jesusgamtello <Jesus.Gamero@alu.uclm.es>
2020-12-05 16:44:47 +0100

    pylint changes

--- L1/ClientAuth.py
+++ L1/ClientAuth.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# pylint: disable=C0114
+# pylint: disable=C0115
+# pylint: disable=C0116
+# pylint: disable=C0103
+# pylint: disable=E0401
+# pylint: disable=W0703
+# pylint: disable=C0413
+# pylint: disable=C0303
 import sys 
 import Ice 
 Ice.loadSlice('icegauntlet.ice')
@@ -12,24 +20,28 @@ import json
 import os
 
 class ClientAuth(Ice.Application):
-    def leer_json(self,fichero,user):
+    def leer_json(self, fichero, user):
+        ''' 
+        method to read a json file 
+        '''
+
         try:
             with open(fichero) as f:
-                usuario=f.read()
-            usuario=json.loads(usuario)
+                usuario = f.read()
+            usuario = json.loads(usuario)
         except:
-            print("Eror, Not found data base")
+            print("Error, Not found data base")
         
-        try :
-            password=usuario[user]['password_hash']
+        try:
+            password = usuario[user]['password_hash']
             return password
         except:
             return None
 
 
-    def run(self,argv):
-        proxy=self.communicator().stringToProxy(argv[1])
-        server= IceGauntlet.AuthenticationPrx.checkedCast(proxy)
+    def run(self, argv):
+        proxy = self.communicator().stringToProxy(argv[1])
+        server = IceGauntlet.AuthenticationPrx.checkedCast(proxy)
 
 
         if not server:
@@ -39,29 +51,29 @@ class ClientAuth(Ice.Application):
         if  len(sys.argv) < 3:
             raise RuntimeError('Error. Parametros insuficientes')
 
-        user= argv[2]
-        password_hash=self.leer_json('users.json',user)
+        user = argv[2]
+        password_hash = self.leer_json('users.json', user)
         print('Enter password: ')
         p = getpass.getpass()
        
-        if password_hash == None:
+        if password_hash is None:
             print('creando nueva contraseña...')
             p = getpass.getpass()
             passHash = hashlib.sha256(p.encode()).hexdigest()
             print(password_hash)
-            server.changePassword(user,None,passHash)
+            server.changePassword(user, None, passHash)
         
-        if len(sys.argv)==4:
+        if len(sys.argv) == 4:
             option = argv[3]
         else:
-            option='d'
+            option = 'd'
 
         if option == 'c':
             p = getpass.getpass()
             passHash = hashlib.sha256(p.encode()).hexdigest()
 
             try:
-                print("---Introducir nueva contraseña---")
+                print("--- Introducir nueva contraseña ---")
                 np = getpass.getpass()
             except Exception as err:
                 print('ERROR:', err)
@@ -69,14 +81,15 @@ class ClientAuth(Ice.Application):
 
             #passHash = hashlib.sha256(p.encode()).hexdigest()
             newpassHash = hashlib.sha256(np.encode()).hexdigest()
-            server.changePassword(user,passHash,newpassHash)
-        elif option == 't' :
+            server.changePassword(user, passHash, newpassHash)
+        elif option == 't':
             passHash = hashlib.sha256(p.encode()).hexdigest()
-            print(server.getNewToken(user,passHash))
+            print(server.getNewToken(user, passHash))
             
 
         elif option == 'd':
-            os.system('python3 ClientServer.py "server -t -e 1.1:tcp -h 192.168.0.15 -p 8700 -t 60000" jesus.gamero "bSOlGteFhvjxLEZQF4nTs7LM0KHcMI1qVEbgEkod" r mi_mapa')
+            os.system('python3 ClientServer.py "server -t -e 1.1:tcp -h 192.168.0.15 -p 8700 \
+                 -t 60000" jesus.gamero "bSOlGteFhvjxLEZQF4nTs7LM0KHcMI1qVEbgEkod" r mi_mapa')
 
 
 ClientAuth().main(sys.argv)
